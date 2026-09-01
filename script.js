6aa7f0b9d6612ff442e491a9f9cbcc6747ff7585
ssunny17 <ssunny7408@gmail.com>
2026-09-01 19:33:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-const loginForm = document.getElementById("loginForm");
+const userIdInput = document.getElementById("userId");
+const passwordInput = document.getElementById("password");
 
-loginForm.addEventListener("submit", function (event) {
+passwordInput.addEventListener("keydown", function (event) {
 
-    // 원래 form의 새로고침 동작 막기
-    event.preventDefault();
+    if (event.key === "Enter") {
 
-    // 입력한 ID와 Password 가져오기
-    const userId = document.getElementById("userId").value.trim();
-    const password = document.getElementById("password").value.trim();
+        event.preventDefault();
 
+        const userId = userIdInput.value.trim();
+        const password = passwordInput.value.trim();
 
-    // 아무것도 입력하지 않은 경우
-    if (userId === "" || password === "") {
+        if (userId === "" || password === "") {
+            alert("아이디와 비밀번호를 입력해주세요.");
+            return;
+        }
 
-        alert("아이디와 비밀번호를 입력해주세요.");
-
-        return;
+        window.location.href = "new-group.html";
     }
-
-
-    // 1번 페이지로 이동
-    window.location.href = "new-group.html";
 
 });
